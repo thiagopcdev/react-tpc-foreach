a74911532ac1dffa237ef1b6d45601547b71cd99
Thiago Prado <thiagopc_dev@hotmail.com>
2024-08-14 16:50:15 -0300

    publishing a patch

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'] })],
+  plugins: [react(), dts({ include: ['lib'], rollupTypes: true })],
   build: {
      lib: {
       entry: path.resolve(__dirname, 'lib/main.ts'),
-      formats: ['es'],
+      formats: ['es']
     },
     rollupOptions: {
         external: ['react', 'react/jsx-runtime']
