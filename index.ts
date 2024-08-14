d8132a1b5407d72bf8b9e5fd66ded7246c14d244
Thiago Prado <thiagopc_dev@hotmail.com>
2024-08-14 16:49:02 -0300

    publishing a patch

--- index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ForEach } from './lib/ForEach';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import path from "path";
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ['lib'] })],
+  build: {
+     lib: {
+      entry: path.resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+        external: ['react', 'react/jsx-runtime']
+    },
+  },
 })
